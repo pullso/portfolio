228cbde9a21322d16a4d25491ff5abc13c7a3394
pullso <pullso.code@gmail.com>
2021-12-14 15:20:46 +0300

    change firebase config

--- quasar.conf.js
+++ quasar.conf.js
@@ -47,7 +47,6 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      env: require('dotenv').config().parsed,
       distDir: ctx.mode.spa ? 'docs' : null,
       // transpile: false,
       // publicPath: '/',
--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -1,7 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
-const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG)
+
+const config = 'eyJhcGlLZXkiOiJBSXphU3lDOEtKcVBLQU05eTIweGV3dkhPcDZZSW9YcS0yckNEMTAiLCJhdXRoRG9tYWluIjoicHVsbHNvLWdpdGh1Yi5maXJlYmFzZWFwcC5jb20iLCJwcm9qZWN0SWQiOiJwdWxsc28tZ2l0aHViIiwic3RvcmFnZUJ1Y2tldCI6InB1bGxzby1naXRodWIuYXBwc3BvdC5jb20iLCJtZXNzYWdpbmdTZW5kZXJJZCI6IjQ0NjQ2NTc0MDE0NCIsImFwcElkIjoiMTo0NDY0NjU3NDAxNDQ6d2ViOmI0ZTY1YTM3MGZjYzU2NTZkMDUzYjUiLCJtZWFzdXJlbWVudElkIjoiRy1TV1JESlJYTEowIn0='
+
+const firebaseConfig = JSON.parse(atob(config))
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
