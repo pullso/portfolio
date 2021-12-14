d6898f51d28d7ff34a0b5beb41342f4facb9d2ea
pullso <pullso.code@gmail.com>
2021-12-14 13:25:20 +0300

    add firebase

---
+++ src/boot/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG)
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
