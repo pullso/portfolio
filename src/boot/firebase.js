e10c18b1410809cba6419512afb1efe616f801b6
pullso <pullso.code@gmail.com>
2021-12-14 13:44:47 +0300

    log firebase config

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -2,6 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
 
+console.log(process.env.FIREBASE_CONFIG, `: firebaseConfig, process.env.FIREBASE_CONFIG`)
+
 const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG)
 
 // Initialize Firebase
