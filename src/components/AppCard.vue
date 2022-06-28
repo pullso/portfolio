17999dd4050232e9ad48f2749fc086bb72c5c447
pullso <pullso.code@gmail.com>
2022-06-28 13:49:47 +0300

    fix chips formatter

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,82 +1,83 @@
 <template>
 
-    <div class="q-pa-md items-start q-gutter-md">
-      <q-card
-        class="my-card col-12 col-md-4"
-        flat
-        bordered
-      >
-        <q-img
-          :src="data.img.includes('https')
+  <div class="q-pa-md items-start q-gutter-md">
+    <q-card
+      class="my-card col-12 col-md-4"
+      flat
+      bordered
+    >
+      <q-img
+        :src="data.img.includes('https')
           ? data.img : 'https://pullso.github.io/' + data.img"
-          loading="lazy"
-          style="height: 300px; width: 100%"
-          fit="fill"
+        loading="lazy"
+        style="height: 300px; width: 100%"
+        fit="fill"
+      />
+      <q-separator/>
+
+      <q-card-section>
+        <q-chip
+          size="md"
+          v-for="(c,idx) in data.categories" :key="c"
+          :color="idx=== 0 ? 'primary' : ''"
+          :text-color="idx===0 ? 'white' : ''"
+        >
+          <div class="text-overline">{{ formatString(c) }}</div>
+        </q-chip>
+        <div class="row justify-between flex-center">
+          <div class="my-card__title text-bold q-my-xs">
+            {{ data.title }}
+          </div>
+        </div>
+      </q-card-section>
+
+      <q-card-actions>
+        <q-btn
+          flat
+          type="a"
+          color="primary"
+          label="Перейти" icon="launch"
+          target="_blank"
+          :href="data.link"
         />
-        <q-separator/>
-
-        <q-card-section>
-          <q-chip
-            dense
-            size="md"
-            v-for="(c,idx) in data.categories" :key="c"
-            :color="idx=== 0 ? 'primary' : ''"
-            :text-color="idx===0 ? 'white' : ''"
-          >
-            <div class="text-overline">{{ c.toUpperCase() }}</div>
-          </q-chip>
-          <div class="row justify-between flex-center">
-            <div class="my-card__title text-bold q-my-xs">{{ data.title }}</div>
-          </div>
-        </q-card-section>
-
-        <q-card-actions>
+        <q-space/>
+        <template v-if="user">
           <q-btn
-            flat
-            type="a"
+            unelevated outline rounded
             color="primary"
-            label="Перейти" icon="launch"
-            target="_blank"
-            :href="data.link"
+            icon="edit"
+            size="small"
+            @click="goEdit"
           />
-          <q-space/>
-          <template v-if="user">
-            <q-btn
-              unelevated outline rounded
-              color="primary"
-              icon="edit"
-              size="small"
-              @click="goEdit"
-            />
-            <q-btn
-              unelevated outline rounded
-              color="primary"
-              icon="content_copy"
-              size="small"
-              @click="copyCard"
-            />
-          </template>
           <q-btn
-            v-if="data"
-            color="grey"
-            rounded
-            flat
-            dense
-            :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
-            @click="expanded = !expanded"
+            unelevated outline rounded
+            color="primary"
+            icon="content_copy"
+            size="small"
+            @click="copyCard"
           />
-        </q-card-actions>
+        </template>
+        <q-btn
+          v-if="data"
+          color="grey"
+          rounded
+          flat
+          dense
+          :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+          @click="expanded = !expanded"
+        />
+      </q-card-actions>
 
-        <q-slide-transition>
-          <div v-show="expanded">
-            <q-separator/>
-            <q-card-section>
-              <div v-html="data.description"></div>
-            </q-card-section>
-          </div>
-        </q-slide-transition>
-      </q-card>
-    </div>
+      <q-slide-transition>
+        <div v-show="expanded">
+          <q-separator/>
+          <q-card-section>
+            <div v-html="data.description"></div>
+          </q-card-section>
+        </div>
+      </q-slide-transition>
+    </q-card>
+  </div>
 </template>
 
 <script>
@@ -90,18 +91,26 @@ export default defineComponent({
   setup(props) {
     const expanded = ref(false)
     const router = useRouter()
+
     function goEdit() {
       router.push(`/edit/${props.data.firestoreId}`)
     }
+
     async function copyCard() {
       const id = String(Date.now())
       await setDoc(doc(db, "cards", id), props.data);
       await router.push(`/edit/${id}`)
     }
+
+    function formatString(string) {
+      return string[0].toUpperCase() + string.substring(1)
+    }
+
     return {
       expanded,
       goEdit,
-      copyCard
+      copyCard,
+      formatString
     }
   },
   props: {
@@ -112,9 +121,7 @@ export default defineComponent({
       type: Object
     }
   },
-  methods: {
-
-  }
+  methods: {}
 })
 </script>
 
